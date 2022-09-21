a4d674cf23e5cffcb6c4d8c885f74c0ae6a6bb13
ss990321 <110081578+ss990321@users.noreply.github.com>
2022-09-21 23:14:33 +0900

    Add files via upload

---
+++ FinalProject/src/main/java/com/example/demo/model/ChatRoom.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
+    private String roomId;
+    private String name;
+
+    private long userCount; // 채팅방 인원수
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
